a4533e7470a5d855f61de92553ab1abcaa79f21d
chick_0 <chick_0@ch1ck.xyz>
2022-03-13 23:42:22 +0900

    fixed...

--- echo.py
+++ echo.py
@@ -11,7 +11,7 @@ from urllib.request import urlopen
 def get_username() -> str:
     try:
         return getlogin()
-    except (OSError, Expection):
+    except (OSError, Exception):
         return system()
 
 
